9904a133de84d061f173db365d2b20cc6fde7d08
Chi Zhang <clvrobj@gmail.com>
2016-04-11 18:03:30 +0800

    use add_clock in Clocks init

--- clocks.py
+++ clocks.py
@@ -8,11 +8,11 @@ FLIP_TIMES = 6 # use next clock after being called FLIP_TIMES times
 class Clocks(object):
 
     def __init__(self):
-        local_zone = get_localzone()
-        local_zone_name = local_zone.zone.split('/')[-1]
-        self.clocks = [(local_zone_name, local_zone)]
+        self.clocks = []
         self.clock_idx = 0
         self.call_times = 0
+        local_zone = get_localzone().zone
+        self.add_clock(local_zone)
 
     def add_clock(self, tz_name):
         tz = timezone(tz_name)
@@ -21,6 +21,8 @@ class Clocks(object):
         return
 
     def get_clock_time_str(self):
+        if len(self.clocks) == 0:
+            return
         if self.call_times % FLIP_TIMES == FLIP_TIMES - 1:
             self.clock_idx += 1
             if self.clock_idx == len(self.clocks):
