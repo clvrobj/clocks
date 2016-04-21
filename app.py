1bba266f06bf6d9f34aaf15fca56049539ca29a8
Chi Zhang <clvrobj@gmail.com>
2016-04-21 09:33:57 +0800

    try add menu time update item

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
 # -*- coding: utf-8 -*-
-import rumps
+from rumps import App, clicked, MenuItem, timer
 from clocks import Clocks
 
 
-class ClockApp(rumps.App):
+class ClockApp(App):
 
     def __init__(self):
         super(ClockApp, self).__init__("Clock")
         self.menu = ["Preferences", "Silly button", "Say hi"]
-        self.clocks = Clocks()
+        self.clocks = Clocks(flip_times=8)
         self.clocks.add_clock('Europe/London')
-        self.clocks.add_clock('Asia/Tokyo')
-        rumps.debug_mode(True)
+        self.clocks.add_clock('Europe/Amsterdam')
+        # self.clocks.add_clock('Asia/Tokyo')
+        # rumps.debug_mode(True)
 
-    @rumps.clicked("Preferences")
-    def prefs(self, _):
-        rumps.alert(message='something', ok='YES!', cancel='NO!')
+    # @rumps.clicked("Preferences")
+    # def prefs(self, _):
+    #     rumps.alert(message='something', ok='YES!', cancel='NO!')
 
-    @rumps.clicked("Silly button")
-    def onoff(self, sender):
-        sender.state = not sender.state
+    # @rumps.clicked("Silly button")
+    # def onoff(self, sender):
+    #     sender.state = not sender.state
 
-    @rumps.clicked("Say hi")
-    def sayhi(self, _):
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
+    # @rumps.clicked("Say hi")
+    # def sayhi(self, _):
+    #     rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
 
-    @rumps.timer(1)
-    def update_time(self, _):
+    @timer(1)
+    def update(self, _):
         self.title = self.clocks.get_clock_time_str()
+
+    # @clicked('Update time')
+    def update_time(self, sender):
+        print 'hello {}'.format(sender)
 
 
 if __name__ == "__main__":
-    ClockApp().run()
+    app = ClockApp()
+    app.menu = [
+        [MenuItem('Update time', callback=app.update_time), ('5s', '10s', '30s')]
+    ]
+    app.run()
--- clocks.py
+++ clocks.py
@@ -7,23 +7,24 @@ FLIP_TIMES = 6 # use next clock after being called FLIP_TIMES times
 
 class Clocks(object):
 
-    def __init__(self, tz_name=''):
+    def __init__(self, tz_name='', flip_times=FLIP_TIMES):
         self.clocks = []
         self.clock_idx = 0
         self.call_times = 0
+        self.flip_times = flip_times
         tz_name = tz_name or get_localzone().zone
         self.add_clock(tz_name)
 
     def add_clock(self, tz_name):
         tz = timezone(tz_name)
-        display_name = tz.zone.split('/')[-1]
+        display_name = tz.zone.split('/')[-1].replace('_', ' ')
         self.clocks.append({'tz':tz, 'display_name':display_name})
         return
 
     def get_clock_time_str(self):
         if len(self.clocks) == 0:
             return
-        if self.call_times % FLIP_TIMES == FLIP_TIMES - 1:
+        if self.call_times % self.flip_times == self.flip_times - 1:
             self.clock_idx += 1
             if self.clock_idx == len(self.clocks):
                 self.clock_idx = 0
